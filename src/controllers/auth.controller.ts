23bdac8dfa5b5a4c0aa15d6264d352567f8d543c
Syahrul Rizal <syahrul.rizal408@gmail.com>
2026-04-30 16:16:28 +0700

    add swagger docs

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -62,6 +62,12 @@ export default {
   },
 
   async login(req: Request, res: Response) {
+    /**
+     #swagger.requestBody = {
+      required: true,
+      schema: {$ref: "#components/schemas/LoginRequest"}
+     }
+     */
     const {
       identifier,
       password
@@ -113,6 +119,11 @@ export default {
   },
 
   async me(req: IReqUser, res:Response) {
+    /**
+     #swagger.security = [{
+       "bearerAuth": []
+     }]
+     */
     try {
       const user = req.user;
 
