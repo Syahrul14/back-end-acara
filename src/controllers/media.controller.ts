6b2de28a70bc95b0aac326f9eb56098b966b35f4
Syahrul Rizal <syahrul.rizal408@gmail.com>
2026-05-17 18:54:17 +0700

    api media handling

---
+++ src/controllers/media.controller.ts
@@ -0,0 +1,71 @@
+import { Response } from "express";
+import { IReqUser} from "../utils/interfaces";
+import uploader from "../utils/uploader";
+
+export default {
+  async single(req:IReqUser, res:Response) {
+    if (!req.file) {
+      return res.status(400).json({
+        message: "File is not exist",
+        data: null
+      });
+    }
+
+    try {
+      const result = await uploader.uploadSingle(
+        req.file as Express.Multer.File
+      );
+
+      res.status(200).json({
+        message: "Success upload a file",
+        data: result
+      })
+    } catch {
+      res.status(500).json({
+        message: "Failed upload a file",
+        data: null
+      })
+    }
+  },
+  async multiple(req:IReqUser, res:Response) {
+    if (!req.files || req.files.length === 0) {
+      return res.status(400).json({
+        message: "File are not exist",
+        data: null
+      });
+    }
+
+    try {
+      const result = await uploader.uploadMultiple(
+        req.files as Express.Multer.File[]
+      );
+
+      res.status(200).json({
+        message: "Success upload files",
+        data: result
+      })
+    } catch {
+      res.status(500).json({
+        message: "Failed upload files",
+        data: null
+      })
+    }
+  },
+  async remove(req:IReqUser, res:Response) {
+    try {
+      const { fileUrl } = req.body as { fileUrl: string };
+      const result = await uploader.remove(fileUrl);
+
+      res.status(200).json({
+        message: "Success remove a file",
+        data: result
+      });
+
+    } catch {
+      res.status(500).json({
+        message: "Failed remove a file",
+        data: null
+      })
+    }
+  },
+};
